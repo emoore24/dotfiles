f2ffaa38dcc297ac710b10bd70173c8ec22a2935
Evan Moore <evan.moore24@gmail.com>
2020-04-26 13:59:37 -0700

    further modify install script

--- install.sh
+++ install.sh
@@ -1,9 +1,15 @@
+echo "PREREQUISITES: "
+echo "Make sure you have installed Neovim"
+
+read -p "Press Enter to Continue, or Ctrl-C to exit" continuevar
+
 BUNDLE_DIR=~/.vim/bundle
 # "dirname" gives directory name of script (BASH_SOURCE). cd to directory and run pwd to get directory of this script
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "Running in $SCRIPT_DIR"
 
+echo "Symlinking files..."
 #Symlink files
 ln -sfv "$SCRIPT_DIR/.vimrc" ~ # vimrc
 ln -sfv "$SCRIPT_DIR/.tmux.conf" ~ # tmux.conf
@@ -13,11 +19,33 @@ ln -sfv "$SCRIPT_DIR/.vimrc" ~/.config/nvim/init.vim # neovim
 mkdir -pv ~/.vim
 ln -sfv ~/.vim ~/.config/nvim
 
-# Uncomment below to install/update Vundle, but I switched to vim-plug.
-# vim-plug is auto installed upon first opening of vim (see my .vimrc)
-#mkdir -p "$BUNDLE_DIR" && (git clone https://github.com/VundleVim/Vundle.vim "$BUNDLE_DIR/Vundle.vim" || (cd "$BUNDLE_DIR/Vundle.vim" && git pull origin master))
+echo "Instaling vim-plug..."
+# Install vim-plug
+# Vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# Uncomment below to install Vundle bundles
-#vim +PluginInstall +qall
+# Neovim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo "instaling Neovim providers..."
+sudo python3 -m pip install --user --upgrade pynvim
+gem install neovim
+
+
+echo "PlugInstall vim-plug bundles"
+# Install vim-plug bundles
+vim +PlugInstall +qall
+nvim +PlugInstall +qall
+
+echo "Install NVM..."
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | zsh
+
+echo "Installing OhMyZsh..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "Things to do:"
+echo "1. Restart shell & npm install neovim, typescript, javascript-typescript-langserver"
+echo "2. Install Homebrew"
+echo "3. Set up zsh using example zshrc in repo."
 
 cd -
